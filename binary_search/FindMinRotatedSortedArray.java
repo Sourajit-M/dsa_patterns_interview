acad7c8295099a9ec0540d7436a86c3a5b723d94
Sourajit-M <sourajitm19@gmail.com>
2026-09-22 12:09:56 +0530

    add Find Minimum in Rotated Sorted Array and Valid Parenthesis classes

---
+++ binary_search/FindMinRotatedSortedArray.java
@@ -0,0 +1,23 @@
+package binary_search;
+
+public class FindMinRotatedSortedArray {
+    public static int findMin(int[] nums){
+        int low = 0, high = nums.length - 1;
+
+        while(low < high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(findMin(nums));
+    }
+}
---
+++ stack/ValidParenthesis.java
@@ -0,0 +1,34 @@
+package stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '{' || ch == '[' || ch == '('){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty())
+                    return false;
+                
+                char top = stack.pop();
+                if(
+                    ch == ']' && top != '[' ||
+                    ch == '}' && top != '{' ||
+                    ch == ')' && top != '('
+                ){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "[()[]{}]";
+        System.out.println(isValid(s));
+    }
+}
